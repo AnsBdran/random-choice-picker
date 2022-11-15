2b31868edb3e7f2415c052dd9cc65c1b51065791
anasBadran <ansaccts@gmail.com>
2022-11-15 14:59:10 +0200

    modified some of the functionality

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 const textArea = document.querySelector("textarea");
 const randomItem = (arr) => Math.floor(Math.random() * arr.length);
+const randomNum = (n) => Math.ceil(Math.random() * n * 100);
 const tags = document.querySelector(".tags");
+
+textArea.focus();
 
 window.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     const items = doThat();
-    items.length && randomClass(items);
+    if (items.length) {
+      items.forEach((item) => item.classList.add("temp"));
+
+      setTimeout(() => {
+        items.forEach((item) => item.classList.remove("temp"));
+        randomClass(items);
+      }, 2000);
+    }
   }
 });
 
@@ -14,15 +24,14 @@ const doThat = (_) => {
   const tagsArr = [];
   const words = textArea.value.split(",").filter((item) => item.trim());
   if (words.length) {
-    console.log(words);
     words.forEach((word) => {
       const tag = document.createElement("span");
       tag.textContent = word;
       tagsArr.push(tag);
       tags.appendChild(tag);
       textArea.value = "";
-      return tagsArr;
     });
+    return tagsArr;
   } else {
     tags.textContent = "You MUST Enter some Valid Values";
     textArea.value = "";
@@ -34,5 +43,3 @@ const randomClass = (tags) => {
   const word = tags[randomItem(tags)];
   word.classList.add("active");
 };
-
-textArea.addEventListener("input", (_) => {});
